11274e4825e5509151f588c808dc7a0f8985dd4a
Jon Michael <jmvlu27@hotmail.com>
2022-02-12 05:14:16 -0500

    define variables and cached element references

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const cells = ['rbc','neutrophil', 'lymphocyte', 'monocyte', 'basophil', 'eosinophil', 'platelet', 'virus']
 
 
 /*-------------------------------- Variables --------------------------------*/
 
-
+let reels = []
 
 /*------------------------ Cached Element References ------------------------*/
-
+let winnings = document.querySelector("#winnings")
+let credits = document.querySelector("#credits")
+let play = document.querySelector("#play")
+let cashOut = document.querySelector("#cashOut")
+let infuse = document.querySelector("#infuse")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
