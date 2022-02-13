84b835675b073dde0b5cd85204889fc041b28a25
Jon Michael <jmvlu27@hotmail.com>
2022-02-13 16:43:53 -0500

    include a deduction from credts  condition if win is due to a virus

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*-------------------------------- Constants --------------------------------*/
-const cellsArr = [{cell : 'rbc', prize: 1000}, {cell : 'neutrophil', prize: 700}, {cell : 'lymphocyte', prize: 600}, {cell : 'monocyte', prize: 500}, {cell : 'basophil', prize: 400}, {cell : 'eosinophil', prize: 300}, {cell : 'platelet', prize: 200}, {cell : 'virus', prize: -500}]
+// const cellsArr = [{cell : 'rbc', prize: 1000}, {cell : 'neutrophil', prize: 700}, {cell : 'lymphocyte', prize: 600}, {cell : 'monocyte', prize: 500}, {cell : 'basophil', prize: 400}, {cell : 'eosinophil', prize: 300}, {cell : 'platelet', prize: 200}, {cell : 'virus', prize: -500}]
 
-console.log(cellsArr)
+const cellsArr = [{cell : 'rbc', prize: 100}, {cell : 'virus', prize: -500}, ]
 
 
 /*-------------------------------- Variables --------------------------------*/
@@ -32,7 +32,7 @@ function init() {
     amount = 0
     winnings.innerHTML = null
     // render()
-    console.log(init)
+    
   }
   
   // function render(){
@@ -75,12 +75,18 @@ play.addEventListener('click', gamble)
       wheel3.innerHTML = randomCell3.cell
       // console.log(randomCell1)
       if (randomCell1 === randomCell2 && randomCell2 === randomCell3) {
-        console.log("you win!!")
-          
-          winnings.innerText = `Congratulations! 3 ${randomCell1.cell}s wins ${randomCell1.prize}!!`
+        
+          if (randomCell1.cell === "virus") {
+            winnings.innerText = `Oh no! a ${randomCell1.cell} has caused a ${randomCell1.prize}cc hemmorhage!!`
+          } else {
+            winnings.innerText = `Congratulations! 3 ${randomCell1.cell}s wins ${randomCell1.prize}!!`
+          }  
 
           amount =  amount + randomCell1.prize
-          console.log(randomCell1.prize)
+            if (amount < 0) {
+              amount = 0
+            }
+          
           
           updateCreditDisplay()
         }
